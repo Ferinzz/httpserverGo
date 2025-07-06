ab9fcabbde99b55884583da1b5e56c714dc031e0
Ferinzz <44227010+Ferinzz@users.noreply.github.com>
2025-07-06 14:58:02 +0300

    D: server file path set to serve index at root access

--- Chirpy/main.go
+++ Chirpy/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	
+	//"fmt"
 	"log"
 	"net/http"
 )
+
+//type apiHandler struct{}
 
 func main() {
 	const port = "8080"
@@ -15,6 +17,17 @@ func main() {
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
+	/*
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/" {
+			http.NotFound(w, req)
+			return
+		}
+	fmt.Fprintf(w, "Welcome to the home page!")
+	})*/
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
