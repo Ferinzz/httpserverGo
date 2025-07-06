4560fe9f80d032b6756237f79255929ff6e9ad55
Ferinzz <44227010+Ferinzz@users.noreply.github.com>
2025-07-06 22:32:37 +0300

    parse the msg and find where all the profanity is. Also loop through the final index to begin building the new string from slices. TODO: add **** between segments and handle start/ending profanes.

--- Chirpy/main.go
+++ Chirpy/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"sync/atomic"
 	"encoding/json"
+	"strings"
+	"sort"
 )
 
 //type apiHandler struct{}
@@ -50,9 +52,6 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
-//**********\\
-//***APIs***\\
-//**********\\
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -61,6 +60,10 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+//*************\\
+//****Admin****\\
+//*************\\
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
 	
@@ -92,6 +95,12 @@ func (cfg *apiConfig) handlerAdminMetrics(w http.ResponseWriter, req *http.Reque
 	</html>
 	`, cfg.fileserverHits.Load())))
 }
+
+
+
+//**********\\
+//***APIs***\\
+//**********\\
 
 func ValidateJSON(w http.ResponseWriter, req *http.Request) {
 	type parameter struct {
@@ -140,6 +149,9 @@ func ValidateJSON(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+
+	CheckforProfane(params.Body)
+
 	type success struct {
 		Valid bool `json:"valid"`
 	}
@@ -157,3 +169,62 @@ func ValidateJSON(w http.ResponseWriter, req *http.Request) {
 	w.Write(dat)
 	return
 }
+
+
+func CheckforProfane(msg string) {
+	profane:= []string{
+    	"kerfuffle",
+    	"sharbert",
+    	"fornax",
+		"12358",
+	}
+
+	var loc []int
+	var indexes []int
+
+	lowered:= strings.ToLower(msg)
+	
+	loc1:=-1
+	loc2:=0
+	for _, word := range profane {
+		log.Print(word)
+		loc2= strings.Index(lowered[loc1+1:], word)
+		//loc1+=loc2
+		//log.Printf("location: %v",loc1)
+		//loc = append(loc, loc1)
+
+		
+		for loc2 != -1 {
+			log.Printf("found: %v",loc2)
+			loc1+=loc2+1
+			loc = append(loc, loc1)
+			indexes = append(indexes, loc1, loc1+len(word))
+			log.Printf("idexes: %v", indexes)
+			log.Print(word)
+			loc2 = strings.Index(lowered[loc1+1:], word)
+			log.Printf("location: %v",loc1)
+		}
+		loc1=-1
+		loc2=0
+		log.Printf("%v",loc)
+		loc=nil
+	}
+	sort.Ints(indexes)
+	log.Printf("idexes: %v", indexes)
+	log.Printf("%v",loc)
+	start:=0
+	end:=len(indexes)
+	if indexes[0] == 0 {
+		start=1
+		end-=1
+	}
+	for i:=start; i<end; i+=2 {
+		log.Printf("first value: %d; second value: %d", i, i+1)
+		log.Printf("segment: %v", msg[indexes[i]:indexes[i+1]])
+	}
+	
+	log.Printf("segment: %v", msg[indexes[len(indexes)-1]:])
+
+}
+
+//kerfuffle fdafdhfkdl sharbert afkljd akl;jkjklfd;ja fornax k;lkj kerfuffle
